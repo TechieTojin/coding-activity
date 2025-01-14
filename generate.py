2c9a02ae54d4ae6ae68560697d74edb3d9356a2c
TechieTojin <tojinsimson28@gmail.com>
2025-01-14 12:00:00 +0530

    Extra contribution 13-1

--- generate.py
+++ generate.py
@@ -6,15 +6,16 @@ start = datetime(2025, 1, 1)
 for i in range(100):
     date = start + timedelta(days=i)
 
-    with open("activity.txt", "a") as f:
-        f.write(f"Contribution {i}\n")
+    for j in range(3):  # add 3 more per day
+        with open("activity.txt", "a") as f:
+            f.write(f"Extra {i}-{j}\n")
 
-    os.system("git add activity.txt")
+        os.system("git add activity.txt")
 
-    d = date.strftime("%Y-%m-%dT12:00:00")
-    os.environ["GIT_AUTHOR_DATE"] = d
-    os.environ["GIT_COMMITTER_DATE"] = d
+        d = date.strftime("%Y-%m-%dT12:00:00")
+        os.environ["GIT_AUTHOR_DATE"] = d
+        os.environ["GIT_COMMITTER_DATE"] = d
 
-    os.system(f'git commit -m "Contribution {i}"')
+        os.system(f'git commit -m "Extra contribution {i}-{j}"')
 
 print("Done!")
